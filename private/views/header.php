b27c5114587851daa04a133a995726bc8fad28ee
Bjørn Emmaneel <27728@ma-web.nl>
2019-05-17 08:45:37 +0200

    Update

---
+++ private/views/header.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Bjørn Emmaneel">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="../../public/css/master.css">
+    <title>Mt Pantheon</title>
+</head>
+<body>
+<div class="container">
+    <nav class="navbar">
+        <div class="navbar-items">
+            <div class="navbar-img">
+                <img src="#" alt="#">
+            </div>
+            <div class="navbar-input">
+                <input class="navbar-search" type="text" placeholder="Search bar">
+            </div>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="header.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pantheons</a>
+                </li>
+                <li class="nav-item login">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+</body>
+</html>
