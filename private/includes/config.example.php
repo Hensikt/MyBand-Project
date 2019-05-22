4b7303c0c5ba43ff9911f6b14b2cc2be10193d05
Bjørn Emmaneel <27728@ma-web.nl>
2019-05-22 14:05:48 +0200

    update

--- private/includes/config.example.php
+++ private/includes/config.example.php
@@ -5,8 +5,5 @@ $config = [
     'DB_NAME' => 'thePantheons',
     'DB_USER' => 'root',
     'DB_PASSWORD' => '',
-    'ROOT' => dirname(dirname(__DIR__)),
-    'PRIVATE' => dirname(__DIR__) . '/private',
-    'WEBROOT' => dirname(__DIR__) . '/public',
 ];
 return $config;
