30bb033b7269199bb4e8c486805d76937605c1bc
Bjørn Emmaneel <27728@ma-web.nl>
2019-06-03 09:31:22 +0200

    update
    update

---
+++ private/controllers/AboutUsController.php
@@ -0,0 +1,12 @@
+<?php
+
+class AboutUsController{
+
+	function About_Us(){
+		
+		include __DIR__ . "/../views/header.php";
+		include __DIR__ . "/../views/About-Us.php";
+		include __DIR__ . "/../views/footer.php";
+		
+	}
+}
