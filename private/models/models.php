67cb134f280bbc287e806657ae8717fdc6359f67
Hensikt <42713950+Hensikt@users.noreply.github.com>
2019-06-13 13:52:19 +0200

    Add files via upload

--- private/models/models.php
+++ private/models/models.php
@@ -6,4 +6,12 @@
  * Time: 13:37
  */
 
+function get_foto($id){
+    $pdo = dbConnect();
+    $sql = "SELECT name FROM fotos WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
 
+    $stmt->execute(['id' => $id]);
+    $foto = $stmt->fetchColumn();
+    return $foto;
+}
